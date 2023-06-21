b1254a3ab4d6a2ba540cb89ca21dee1e3a96e83a
Linus Walker <w000alker@gmail.com>
2023-06-21 08:58:59 +0630

    V1??

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::path::Path;
+
+use serde::{de::Deserialize, ser::Serialize};
+use sled::{Db, IVec, Result};
+
+pub struct JsonDb {
+    db: Db,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl JsonDb {
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<JsonDb> {
+        let db = sled::open(path)?;
+        Ok(JsonDb { db })
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn insert<K: AsRef<[u8]>, V: ?Sized + Serialize>(
+        &self,
+        key: K,
+        value: &V,
+    ) -> Result<Option<IVec>> {
+        let value = serde_json::to_string(value).unwrap();
+        Ok(self.db.insert(key, value.as_bytes())?)
+    }
+
+    pub fn get<T: for<'a> Deserialize<'a>>(
+        &self,
+        key: &dyn AsRef<[u8]>,
+    ) -> std::result::Result<T, serde_json::Error> {
+        let v = self.db.get(key.as_ref()).unwrap().unwrap();
+        let x = String::from_utf8(v.to_vec()).unwrap();
+        serde_json::from_str(&x)
     }
 }
